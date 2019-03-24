14a4a10d683ec31ab30fbee9ccaf44ac07ce1164
BartoszWisniewski <bartosz.wisniewski04@gmail.com>
2019-03-24 12:49:26 +0100

    testy part 2

--- src/test/java/userTest/service/LoginServiceTest.java
+++ src/test/java/userTest/service/LoginServiceTest.java
@@ -3,11 +3,7 @@ package userTest.service;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import userTest.dao.UserDAO;
-import userTest.data.User;
 import userTest.dto.UserDTO;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,23 +13,9 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class LoginServiceTest {
 
-    private static final User USER = newUser("Admin", "HASLO", "Ala", "OLALA", "123456", 2);
-
-
     private static final String log = "Admin";
 
     private static final String pass = "HASLO";
-
-    private static User newUser(String Login, String Password, String Name, String Surname, String Telephone, Integer UserRole) {
-        User u = new User();
-        u.setLogin(Login);
-        u.setPassword(Password);
-        u.setName(Name);
-        u.setSurname(Surname);
-        u.setTelephone(Telephone);
-        u.setUserRole(UserRole);
-        return u;
-    }
 
     private static final UserDTO USER_DTO = newUserDTO("Admin", "Ala", "OLALA", "123456", 2);
 
@@ -47,12 +29,6 @@ public class LoginServiceTest {
         return u1;
     }
 
-
-    /*@Mock
-    private UserDAO userDAO;*/
-
-    @InjectMocks
-    private LoginService loginService = new LoginService();
 
     LoginService test = mock(LoginService.class);
 
@@ -78,19 +54,15 @@ public class LoginServiceTest {
         assertThat(test.checkIfUserCanLogin(log, pass)).isEqualTo(false);
 
     }
-    
+
 
     @Test
-    public void loggedUser() {
-    /*    //given
-        Mockito.when(loginService.loggedUser(log)).thenReturn(USER_DTO);
-        Mockito.when(userDAO.findByLog(log)).thenReturn(USER);
-        //when
-        //then
-    //    assertThat(out).isEqualTo(true);
-*/
+    public void dataLoggedUser() {
+
         LoginService test = mock(LoginService.class);
+        //when
         when(test.loggedUser(log)).thenReturn(USER_DTO);
+        //then
         assertThat(test.loggedUser(log)).isEqualTo(USER_DTO);
     }
 }
